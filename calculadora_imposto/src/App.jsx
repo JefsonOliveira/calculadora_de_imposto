cf390f6482158cc93a491213b8f4b5c1c43ca12d
jefsonoliveira <kaironoliveira16@gmail.com>
2024-06-26 16:11:48 -0300

    feat: Validação dos formúlarios com formik

---
+++ calculadora_imposto/src/App.jsx
@@ -0,0 +1,26 @@
+import TaxForm from "./components/TaxForm";
+import TaxReport from "./components/TaxReport";
+
+import { Container, Typography } from "@mui/material";
+
+import { useState } from "react";
+
+function App() {
+  const [taxData, setTaxData] = useState(null);
+
+  const calculateTax = (values) => {
+
+  }
+
+  return (
+    <Container style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <Typography variant="h3" align="center" style={{ marginBottom: "16px" }}>
+        Calculadora de Impostos
+      </Typography>
+      <TaxForm onSubmit={calculateTax} />
+      {taxData && <TaxReport />}
+    </Container>
+  );
+}
+
+export default App;
